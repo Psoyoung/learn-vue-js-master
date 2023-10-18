0b375a46461945664e4cb3021da91a66a99f6cc7
DESKTOP-JJHMMDM\user <6545742@naver.com>
2023-10-18 17:53:51 +0900

    install vue-cli

---
+++ vue-cli/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    {{ str }}
+    <app-header v-bind=""></app-header>
+  </div>
+</template>
+
+<script>
+import AppHeader from './components/AppHeader.vue';
+
+export default{
+  data: function() {
+    return {
+      str : 'Header'
+    }
+  },
+  components: {
+    'app-header': AppHeader
+  }
+}
+</script>
