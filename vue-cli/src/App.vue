9276aaac2575de8ce6af85317eb8469911965ce1
soyoung <6545742@naver.com>
2023-10-23 17:49:51 +0900

    install vue-form

--- vue-cli/src/App.vue
+++ vue-cli/src/App.vue
@@ -1,21 +1,38 @@
 <template>
   <div>
-    {{ str }}
-    <app-header v-bind=""></app-header>
+    <!-- <app-header v-bind:프롭스 속성 이름="상위 컴포넌트의 데이터 이름"></app-header> -->
+    <app-header v-bind:propsdata="str" v-on:renew="renewStr"></app-header>
   </div>
 </template>
 
 <script>
 import AppHeader from './components/AppHeader.vue';
 
-export default{
-  data: function() {
+// var AppHeader = {
+//   template: '<header><h1>Header</h1></header>'
+// }
+
+// new Vue({
+//   data: {
+//     str: 'hi'
+//   }
+// })
+
+export default {
+  data: function () {
     return {
-      str : 'Header'
+      str: 'Header'
     }
   },
   components: {
     'app-header': AppHeader
+  },
+  methods: {
+    renewStr: function () {
+      this.str = 'hi';
+    }
   }
 }
 </script>
+
+<style></style>
